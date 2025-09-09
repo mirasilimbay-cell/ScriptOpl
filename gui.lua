458d9c8f65eee337c11f28d3dc576ca9671ac632
mirasilimbay-cell <mirasilimbay@mail.ru>
2025-09-09 18:18:56 +0500

    Create gui.lua

---
+++ gui.lua
@@ -0,0 +1,18 @@
+if game.Placeid == 137595477352660 then
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()  
+local Window = OrionLib:MakeWindow({Name = "8ball hub ‖ One Piece: Mythical", HidePremium = false,introText = "8ball hub", SaveConfig = true, ConfigFolder = "nothing"}
+
+local FarmTab = Window:MakeTab({
+	Name = "Autofruits",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+
+
+
+
+
+
+end
+OrionLib:Init()
